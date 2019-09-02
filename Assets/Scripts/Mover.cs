1414cca59848ed1d3b314b65ce9750085b66529c
bagnal401 <bagnal401@morrisville.edu>
2019-09-02 11:31:19 -0400

    Add S2L16 Finished pre-release.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -16,7 +16,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
-        
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -28,5 +28,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
