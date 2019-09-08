9ebec5fecc7090f19d01797930e4d3ce93d2dbb2
bagnal401 <bagnal401@morrisville.edu>
2019-09-08 14:35:22 -0400

    Add ActionScheduler script to remove Fighter and Mover circle dependency

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using RPG.Movement;
+using RPG.Core;
 
 namespace RPG.Combat
 {
@@ -32,6 +33,7 @@ namespace RPG.Combat
 
         public void Attack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
         }
 
