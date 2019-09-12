be89ef9db5dd649ed9f3ae58086e9aa83b845e58
bagnal401 <bagnal401@morrisville.edu>
2019-09-11 23:09:30 -0400

    Add death animation, add character parent prefab and enemy and player child prefabs, add health points, look at target, and stop attack

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -11,9 +11,10 @@ namespace RPG.Combat
         // Configurations
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         // Target should be able to change position at least.
-        Transform target;
+        Health target;
         float timeSinceLastAttack = 0;
 
         private void Update()
@@ -24,36 +25,46 @@ namespace RPG.Combat
             // then movement will cancel.
             if (target == null) return;
 
+            if (target.IsDead()) return;
+
             // If the player is not in range of the target after clicking attack,
             // then the player will move to be in range.
             if (!GetIsInRange())
             {
-                GetComponent<Mover>().MoveTo(target.position);
+                GetComponent<Mover>().MoveTo(target.transform.position);
             }
             // Otherwise the player's movement towards the target will cancel
             // and regular movement will begin.
             else
             {
                 GetComponent<Mover>().Cancel();
-                AttackBehaviour();
+                AttackBehavior();
             }
         }
 
-        private void AttackBehaviour()
+        private void AttackBehavior()
         {
+            transform.LookAt(target.transform);
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
+                // This will trigger the Hit event.
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
             }
 
         }
 
+        // Animation event
+        void Hit()
+        {
+            target.TakeDamage(weaponDamage);
+        }
+
         // Calls GetIsInRange, which wil determine if the player's position is
         // less than the weapon range.
         private bool GetIsInRange()
         {
-            return Vector3.Distance(transform.position, target.position) < weaponRange;
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
         // Calls the Attack method, which will call the ActionScheduler component
@@ -61,19 +72,16 @@ namespace RPG.Combat
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
-            target = combatTarget.transform;
+            target = combatTarget.GetComponent<Health>();
         }
 
         // Calls the Cancel method, which will cancel movement when the target is null.
         public void Cancel()
         {
+            GetComponent<Animator>().SetTrigger("stopAttack");
             target = null;
         }
 
-        // Animation event
-        void Hit()
-        {
 
-        }
     }
 }
