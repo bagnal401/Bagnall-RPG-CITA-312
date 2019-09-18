d93c5a587d174b0b30dc21c2077990baf0012bbe
bagnal401 <bagnal401@morrisville.edu>
2019-09-18 17:32:37 -0400

    Add AIController script and ability for enemies to chase the player. S5L46 done

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -77,7 +77,7 @@ namespace RPG.Combat
             return Vector3.Distance(transform.position, target.transform.position) < weaponRange;
         }
 
-        public bool CanAttack(CombatTarget combatTarget)
+        public bool CanAttack(GameObject combatTarget)
         {
             if (combatTarget == null)
             {
@@ -89,7 +89,7 @@ namespace RPG.Combat
 
         // Calls the Attack method, which will call the ActionScheduler component
         // to begin combat at this target.
-        public void Attack(CombatTarget combatTarget)
+        public void Attack(GameObject combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.GetComponent<Health>();
