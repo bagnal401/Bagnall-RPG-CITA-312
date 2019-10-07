8dc566ed5f3aa4bb4d7fe35c2dc03715a939bc0a
bagnal401 <bagnal401@morrisville.edu>
2019-10-07 15:48:48 -0400

    Add CinematicsControlRemover script to game. S6L66 done.

--- Assets/Scripts/Cinematics/cinematicsControlRemover.cs
+++ Assets/Scripts/Cinematics/cinematicsControlRemover.cs
@@ -2,25 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
 
 namespace RPG.Cinematics
 {
     public class cinematicsControlRemover : MonoBehaviour
     {
+        GameObject player;
+
         private void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
+            GameObject player = GameObject.FindWithTag("Player");
         }
 
         void DisableControl(PlayableDirector pd)
         {
-            print("DisableControl");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector pd)
         {
-            print("EnableControl");
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
