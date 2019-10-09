d738be119ee01e494ba6df9f9bbf3ad24974b865
bagnal401 <bagnal401@morrisville.edu>
2019-10-09 00:58:28 -0400

    Add Fader script to game. S7L75 done.

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOut(3f));
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            print("Faded out");
+            yield return FadeIn(3f);
+            print("Faded in");
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) // alpha is not 1
+            {
+                // moving alpha toward 1
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) // alpha is not 1
+            {
+                // moving alpha toward 1
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+}
