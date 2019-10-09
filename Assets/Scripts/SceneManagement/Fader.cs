b4e304780657d7ae6b11966e75a5d48a0d17bacf
bagnal401 <bagnal401@morrisville.edu>
2019-10-09 01:46:04 -0400

    Add Persistent object game object with fader to the game. S7L76 done

--- Assets/Scripts/SceneManagement/Fader.cs
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -12,14 +12,14 @@ namespace RPG.SceneManagement
         {
             canvasGroup = GetComponent<CanvasGroup>();
 
-            StartCoroutine(FadeOut(3f));
+            StartCoroutine(FadeOutIn());
         }
 
         IEnumerator FadeOutIn()
         {
             yield return FadeOut(3f);
             print("Faded out");
-            yield return FadeIn(3f);
+            yield return FadeIn(1f);
             print("Faded in");
         }
 
@@ -35,9 +35,9 @@ namespace RPG.SceneManagement
 
         public IEnumerator FadeIn(float time)
         {
-            while (canvasGroup.alpha > 0) // alpha is not 1
+            while (canvasGroup.alpha > 0) // alpha is not 0
             {
-                // moving alpha toward 1
+                // moving alpha toward 0
                 canvasGroup.alpha -= Time.deltaTime / time;
                 yield return null;
             }
--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -16,6 +16,9 @@ namespace RPG.SceneManagement
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform SpawnPoint;
         [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 1f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeWaitTime = 1f;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -33,11 +36,18 @@ namespace RPG.SceneManagement
                 yield break;
             }
 
+            Fader fader = FindObjectOfType<Fader>();
+
             DontDestroyOnLoad(gameObject);
+
+            yield return fader.FadeOut(fadeOutTime);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
 
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
+
+            yield return new WaitForSeconds(fadeWaitTime);
+            yield return fader.FadeIn(fadeInTime);
 
             Destroy(gameObject);
         }
